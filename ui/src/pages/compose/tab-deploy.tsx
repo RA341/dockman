39c272194d9b6ed34d9e72c940b1f60aafe6a183
ra341 <razerishi1505@gmail.com>
2025-12-05 12:49:12 -0500

    added exec options for deploy page

--- ui/src/pages/compose/tab-deploy.tsx
+++ ui/src/pages/compose/tab-deploy.tsx
@@ -1,5 +1,15 @@
 import {useState} from 'react';
-import {Box, Button, Dialog, DialogActions, DialogContent, DialogTitle, Typography} from '@mui/material';
+import {
+    Autocomplete,
+    Box,
+    Button,
+    Dialog,
+    DialogActions,
+    DialogContent,
+    DialogTitle,
+    TextField,
+    Typography
+} from '@mui/material';
 import {ContainerTable} from './components/container-info-table';
 import {getWSUrl} from "../../lib/api.ts";
 import {useDockerCompose} from '../../hooks/docker-compose.ts';
@@ -31,12 +41,31 @@ export function TabDeploy({selectedPage}: DeployPageProps) {
     };
 
     const execContainer = useContainerExec(state => state.execParams)
-    const handleContainerExec = (containerId: string, containerName: string) => {
-        const cmd = "/bin/sh"
+
+    const [showExecDialog, setShowExecDialog] = useState(false)
+    const [containerId, setContainerId] = useState("")
+    const [containerName, setContainerName] = useState("")
+
+    function showDialog(containerId: string, containerName: string) {
+        setContainerName(containerName);
+        setContainerId(containerId);
+        setShowExecDialog(true)
+    }
+
+    const closeExecDialog = () => {
+        setContainerName("");
+        setContainerId("");
+        setShowExecDialog(false)
+    }
+
+    const commandOptions = ["/bin/sh", "/bin/bash", "sh", "bash", "zsh"];
+    const [selectedCmd, setSelectedCmd] = useState<string>('/bin/sh');
+    const handleConnect = (containerId: string, containerName: string, cmd: string) => {
         const encodedCmd = encodeURIComponent(cmd);
         const url = getWSUrl(`docker/exec/${containerId}?cmd=${encodedCmd}`)
         execContainer(`${selectedPage}: exec-${containerName}`, url, true)
-    };
+        closeExecDialog()
+    }
 
     if (!selectedPage) {
         return (
@@ -64,7 +93,7 @@ export function TabDeploy({selectedPage}: DeployPageProps) {
                         setSelectedServices={setSelectedServices}
                         selectedServices={selectedServices}
                         onShowLogs={handleContainerLogs}
-                        onExec={handleContainerExec}
+                        onExec={showDialog}
                     />
                 </Box>
             </Box>
@@ -76,6 +105,45 @@ export function TabDeploy({selectedPage}: DeployPageProps) {
                 </DialogContent>
                 <DialogActions>
                     <Button onClick={closeErrorDialog} color="primary">Close</Button>
+                </DialogActions>
+            </Dialog>
+
+            <Dialog open={showExecDialog} onClose={closeExecDialog}>
+                <DialogTitle>Choose exec entrypoint</DialogTitle>
+                <DialogContent sx={{overflow: 'visible'}}>
+                    <Autocomplete
+                        freeSolo
+                        options={commandOptions}
+                        value={selectedCmd}
+                        onInputChange={(_, value) => setSelectedCmd(value)}
+                        sx={{flex: 1}}
+                        renderInput={(params) => (
+                            <TextField
+                                {...params}
+                                label="Shell Command"
+                                variant="outlined"
+                                size="small"
+                                slotProps={{
+                                    inputLabel: {style: {color: '#aaa'}},
+                                    input: {
+                                        ...params.InputProps,
+                                        style: {color: '#fff', backgroundColor: '#333'}
+                                    }
+                                }}
+                            />
+                        )}
+                    />
+                    <Button
+                        variant="contained"
+                        onClick={() => handleConnect(containerId, containerName, selectedCmd)}
+                        color="primary"
+                        sx={{mt: 2}}
+                    >
+                        Connect
+                    </Button>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={closeExecDialog} color="primary">Close</Button>
                 </DialogActions>
             </Dialog>
         </Box>
