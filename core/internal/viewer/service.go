2480fdb1356906ea86e305926dbe5a8db7c674c4
ra341 <razerishi1505@gmail.com>
2025-12-28 20:08:48 -0500

    fixed viewer network config

--- core/internal/viewer/service.go
+++ core/internal/viewer/service.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 
 	"github.com/RA341/dockman/internal/docker"
+	"github.com/RA341/dockman/internal/info"
 	"github.com/RA341/dockman/pkg/syncmap"
 	"github.com/google/uuid"
 	"github.com/moby/moby/api/types/container"
@@ -58,6 +59,45 @@ func (s *Service) StartSession(ctx context.Context, relPath string, alias string
 	}
 
 	var netConf *network.NetworkingConfig
+	if info.IsDocker() {
+		//log.Debug().Msg("docker detected inspecting container")
+
+		filterArgs := client.Filters{}
+		filterArgs.Add("label", "dockman.container=true")
+
+		list, err := cont.ContainerList(ctx, client.ContainerListOptions{
+			Filters: filterArgs,
+		})
+		if err != nil {
+			return "", nil, err
+		}
+
+		done := false
+		for _, cont := range list.Items {
+			var netName string
+			for name := range cont.NetworkSettings.Networks {
+				netName = name
+				break
+			}
+
+			netConf = &network.NetworkingConfig{
+				EndpointsConfig: map[string]*network.EndpointSettings{
+					netName: {},
+				},
+			}
+			log.Debug().Str("path", fullpath).
+				Str("netname", netName).
+				Msg("using network")
+
+			// we only care about the first dockman container multiple instances are not supported
+			break
+		}
+
+		if !done {
+			return "", nil, fmt.Errorf("could not find dockman container")
+		}
+	}
+
 	image := "ghcr.io/coleifer/sqlite-web:latest"
 	progress, err := cont.ImagePull(ctx, image, client.ImagePullOptions{})
 	if err != nil {
